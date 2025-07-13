687de87f37a55fc76a8e9032783cc5a44cc76801
David <linuslagoo@yahoo.com>
2025-07-13 00:57:43 -0400

    updated error message
    for anyone who doesn't have trash-cli installed, 
    which is for emptying trash

--- mintain.sh
+++ mintain.sh
@@ -28,25 +28,25 @@ Commands include: -update
 \t\t  -empty-trash
 \t\t  -clean 
 \t\t  'dist-upgrade' will prompt (y/n)
-\t\t   option to update again IF upgrades exist (prompts)\n\n
+\t\t   option to update again if/when upgrades exist (prompts)\n\n
 "
 
 read -n 1 -s -p "Press any key to continue.. "
 clear
 
 
-#com1
+#com1- update
 printf "\n${bold}Command 1: ${yellow}update ${nocolor}\n"
 sudo apt-get update && printf "\n"
 
 #start timer
 start=$SECONDS
 
-#com2
+#com2- upgrade
 printf "${bold}Command 2: ${yellow}upgrade ${nocolor}\n"
 sudo apt-get upgrade && printf "\n"
 
-#com3
+#com3- dist-upgrade
 read -p "$(printf ${bold}"Command 3:"${yellow}" dist-upgrade? (y/n) "${nocolor})" -n 1 -r
 printf "\n"
     if [[ $REPLY =~ ^[Yy]$ ]]
@@ -64,45 +64,46 @@ printf "\n"
     fi
 printf "\n"
 
-#com4
+#com4- update-grub
 printf "${bold}Command 4: ${yellow}update-grub ${nocolor}\n"
 sudo update-grub && printf "\n"
 
-#com5
+#com5- autoremove
 printf "${bold}Command 5: ${yellow}autoremove ${nocolor}\n"
 sudo apt-get autoremove && printf "\n"
 
-#com6
+#com6- autoclean
 printf "${bold}Command 6: ${yellow}autoclean ${nocolor}\n"
 sudo apt-get autoclean && printf "\n"
 
-#com7-trash
+#com7- empty-trash
 read -p "$(printf ${bold}"Command 7:"${yellow}" empty trash? (y/n) "${nocolor})" -n 1 -r
 printf "\n"
     if [[ $REPLY =~ ^[Yy]$ ]]
     then
-    trash-empty && printf "" || printf "Skipped. trash-cli not installed\n"
-    printf "Done\n"
+    trash-empty 2>/dev/null || printf "Skipped. [trash-cli not installed]\n"
     fi
 printf "\n"
+sleep 0.5
 
-#com8
+
+#com8- clean
 printf "${bold}Command 8: ${yellow}clean ${nocolor}\n"
 sudo apt-get clean && printf "\n"
+sleep 0.5
 
 #done
 pad="     "
-printf "$pad ${bold}Done ${yellow}and Done.${nocolor} >>> ${bold}All Systems Go ${nocolor}\n\n"
+printf "$pad ${bold}Done ${yellow}and Done${nocolor}" ;  printf " >>> "; sleep 1 ; printf "${bold}All Systems Go ${nocolor}\n\n"
 sleep 0.5
 
 #end timer
 end=$SECONDS
 printf "$pad ${bold}Time ${yellow}elapsed:${red} $((end-start)) seconds ${nocolor}\n\n\n"
-sleep 0.5
-
+sleep 0.2
 
 #return prompt
 padd="          "
 printf "$padd ${yellow}Here's your prompt.. ${nocolor}\n\n"
-sleep 0.2
+sleep 0.5
 
